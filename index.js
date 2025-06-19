49cf57d352d0cdcc865cb59512c377e2e0bb9069
Sumit-rautela <sumitsingh893893@gmail.com>
2025-06-19 21:30:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -11,6 +10,7 @@ const initializeStockfish = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* Pass initializer to App component */}
     <App stockfishInitializer={initializeStockfish} />
   </React.StrictMode>
 );
